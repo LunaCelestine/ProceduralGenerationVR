89bb7ed7d25be4589693a36c163b0e6b67028129
bdabdoub <bdabdoub@gmail.com>
2018-08-08 10:33:42 -0500

    Added character model; Imported Windows Mixed Reality Camera, Applied Mixed Reality setting; Began writing CameraFollow script;

---
+++ VirtuaBlocks/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+
+    [SerializeField] Transform target;
+    [SerializeField] float distance;
+    [SerializeField] float targetHeight;
+
+    private float x = 0;
+    private float y = 0;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+        if(!target) {
+            return;
+        }
+        y = target.eulerAngles.y;
+
+        Quaternion rotation = Quaternion.Euler(x, y, 0);
+        transform.rotation = rotation;
+
+        var position = target.position - (rotation * Vector3.forward * distance + new Vector3(0, -targetHeight, 0));
+
+
+
+	}
+}
